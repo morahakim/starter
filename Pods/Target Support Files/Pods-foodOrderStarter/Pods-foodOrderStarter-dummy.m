bed5fd484f4bb0712f5b5b9b03d7b21579cecdaf
mora hakim <morahakim@moras-MacBook-Pro.local>
2021-10-06 00:11:08 +0700

    menambah pod

---
+++ Pods/Target Support Files/Pods-foodOrderStarter/Pods-foodOrderStarter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_foodOrderStarter : NSObject
+@end
+@implementation PodsDummy_Pods_foodOrderStarter
+@end
---
+++ foodOrderStarter/foodOrderStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodOrderStarterApp.swift
+//  foodOrderStarter
+//
+//  Created by mora hakim on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct foodOrderStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
